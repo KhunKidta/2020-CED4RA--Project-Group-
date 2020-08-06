b543120199506eec23a13e08e932558344a64fd5
KhunKidta <48791730+KhunKidta@users.noreply.github.com>
2020-08-06 09:49:22 +0700

    project01

--- register.php
+++ register.php
@@ -46,47 +46,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register Page</title>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<style>
+    .bs-example{
+        margin: 20px;        
+    }
+</style>
 
 
 </head>
-<body class="text-center">
+<body>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div class="form-group">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <div class="text-center">
+
+    <div class="jumbotron text-center">
+    <h1>Register Page</h1>
+    <p>Please fill out the form to register</p> 
+    </div>
+
         <label for="username">Username</label>
-        <input type="text"  name="username" placeholder="Enter your username" required>
-        </div>
-
-        <div class="form-group">
+        <input type="text" name="username" placeholder="Enter your username" required>
+        <br>
         <label for="password">Password</label>
-        <input type="password"  name="password" placeholder="Enter your password" required>
-        </div>
-
-        <div class="form-group">
+        <input type="password" name="password" placeholder="Enter your password" required>
+        <br>
         <label for="firstname">Firstname</label>
-        <input type="text"  name="firstname" placeholder="Enter your firstname" required>
-        </div>
-
-        <div class="form-group">
+        <input type="text" name="firstname" placeholder="Enter your firstname" required>
+        <br>
         <label for="lastname">Lastname</label>
-        <input type="text"  name="lastname" placeholder="Enter your lastname" required>
-        </div>
+        <input type="text" name="lastname" placeholder="Enter your lastname" required>
+        <br>
         <input type="submit" name="submit" value="Submit">
-    
+        </div>
     </form>
 
+    <div class="text-center">
     <a href="index.php">Go back to index</a>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+  </body>
 </html>
